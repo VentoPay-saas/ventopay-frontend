f571a03d20345d71c6a3cea132102e2e011a0fb3
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-12-04 14:35:43 +0500

    migrated the recaptcha saudkhanbpk@gmail.com and deployed

--- src/configs/app-global.js
+++ src/configs/app-global.js
@@ -6,9 +6,8 @@ export const BASE_URL =
   process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 export const WEBSITE_URL = 'http://localhost:5000';
 // export const BASE_URL =
-//   process.env.REACT_APP_BASE_URL ||
-//   'https://ventopay-backend-q2ty.onrender.com';
-// export const WEBSITE_URL = 'https://ventopay-backend-q2ty.onrender.com';
+//   process.env.REACT_APP_BASE_URL || 'https://ventopay-backend-1.onrender.com';
+// export const WEBSITE_URL = 'https://ventopay-backend-1.onrender.com';
 export const api_url = BASE_URL + '';
 export const api_url_admin = BASE_URL + '/api/v1/dashboard/admin/';
 export const api_url_admin_dashboard = BASE_URL + '/api/v1/dashboard/';
@@ -31,7 +30,7 @@ export const MESSAGING_SENDER_ID = '443842414474';
 export const APP_ID = '1:443842414474:web:00ae149f1ea8affc466a73';
 export const MEASUREMENT_ID = 'G-9EPB4X4TP4';
 
-export const RECAPTCHASITEKEY = '6Le9qmgqAAAAANiF-yj--jO4Gmx-YCjL_svpSwnd';
+export const RECAPTCHASITEKEY = '6LcVDpIqAAAAALcKU_o1LYj-QLJKlrJpE-ZQ5o6e';
 
 export const DEMO_SELLER = 334; // seller_id
 export const DEMO_SELLER_UUID = '3566bdf6-3a09-4488-8269-70a19f871bd0'; // seller_id
