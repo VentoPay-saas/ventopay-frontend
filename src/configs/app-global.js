9470af88885cc64c53c1c65563d9021ca3f132ae
imrookhan44 <khannihar921@gmail.com>
2025-02-18 12:46:12 +0500

    change the backend url also the recaptcha site key

--- src/configs/app-global.js
+++ src/configs/app-global.js
@@ -2,9 +2,12 @@ export const PROJECT_NAME = 'VentoPay-saas';
 // export const BASE_URL =
 //   process.env.REACT_APP_BASE_URL || 'https://saas-api.foodyman.org';
 // export const WEBSITE_URL = 'https://saas-api.foodyman.org';
+// export const BASE_URL =
+//   process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
+// export const WEBSITE_URL = 'http://localhost:5000';
 export const BASE_URL =
-  process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
-export const WEBSITE_URL = 'http://localhost:5000';
+  process.env.REACT_APP_BASE_URL || 'https://ventopay-backend-xdwo.onrender.com';
+export const WEBSITE_URL = 'https://ventopay-backend-xdwo.onrender.com';
 // export const BASE_URL =
 //   process.env.REACT_APP_BASE_URL ||
 //   'https://ventopay-backend-q2ty.onrender.com';
@@ -31,7 +34,7 @@ export const MESSAGING_SENDER_ID = '443842414474';
 export const APP_ID = '1:443842414474:web:00ae149f1ea8affc466a73';
 export const MEASUREMENT_ID = 'G-9EPB4X4TP4';
 
-export const RECAPTCHASITEKEY = '6LcVDpIqAAAAALcKU_o1LYj-QLJKlrJpE-ZQ5o6e';
+export const RECAPTCHASITEKEY = '6Ldlq9oqAAAAAAg16PoO9ZSgSZOrimtqzoYlj8yI';
 
 export const DEMO_SELLER = 334; // seller_id
 export const DEMO_SELLER_UUID = '3566bdf6-3a09-4488-8269-70a19f871bd0'; // seller_id
